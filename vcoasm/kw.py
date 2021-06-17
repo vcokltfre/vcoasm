4d525cea231be8a1de101ff835eed11bf1a41b69
vcokltfre <vcokltfre@gmail.com>
2021-06-17 10:08:59 +0100

    feat: switch keywords to dict

--- vcoasm/kw.py
+++ vcoasm/kw.py
@@ -1,5 +1,16 @@
-keywords = [
-    "ADD", "SUB", "MUL", "DIV", "MOD",
-    "JMP", "JMPP", "JMPZ", "JMPNZ",
-    "LDA", "STA", "OUT", "OUTP", "SUBLEQ",
-]
+keywords = {
+    "ADD":      0x00,
+    "SUB":      0x01,
+    "MUL":      0x02,
+    "DIV":      0x03,
+    "MOD":      0x04,
+    "JMP":      0x05,
+    "LDA":      0x06,
+    "STA":      0x07,
+    "OUT":      0x08,
+    "OUTP":     0x09,
+    "JMPP":     0x0A,
+    "JMPZ":     0x0B,
+    "JMPNZ":    0x0C,
+    "SUBLEQ":   0x0D,
+}
